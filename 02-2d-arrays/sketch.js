69c433e1a5f7c40060bdab003604d6df312cb1c7
Qyjj7 <126735053+Qyjj7@users.noreply.github.com>
2023-03-23 15:20:05 -0600

    tile objects

--- 02-2d-arrays/sketch.js
+++ 02-2d-arrays/sketch.js
@@ -10,11 +10,17 @@ const ROWS = 15;
 const COLS = 15;
 let grid;
 let cellSize;
+let tilesList = [];
+
+let NorthSouthCorridor;
+let EastWestCorridor;
 
 
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
+
+  createTileTypes();
 
   grid = create2dArray(ROWS, COLS);
 
@@ -35,11 +41,34 @@ function draw() {
 }
 
 
+function createTileTypes() {
+
+  NorthSouthCorridor = {
+    color: "blue",
+  };
+  tilesList.push(NorthSouthCorridor);
+
+  EastWestCorridor = {
+    color: "red",
+  };
+  tilesList.push(EastWestCorridor);
+}
+
+function randomTile() {
+  return tilesList[random(tilesList.length-1)];
+}
+
 function displayGrid(grid) {
 
   for (let y = 0; y < ROWS; y++) {
     for (let x = 0; x < COLS; x ++) {
 
+      if (grid[y][x] !== "blank") {
+        fill(grid[y][x].color);
+      }
+      if (grid[y][x] === "blank") {
+        noFill();
+      }
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
   }
@@ -47,6 +76,7 @@ function displayGrid(grid) {
 
 
 function create2dArray(ROWS, COLS) {
+
   let newGrid = [];
   for (let y = 0; y < ROWS; y++) {
     newGrid.push([]);
@@ -55,4 +85,24 @@ function create2dArray(ROWS, COLS) {
     }
   }
   return newGrid;
+}
+
+
+function exploreCell(x, y) {
+
+  if (x >= 0 && x < COLS && y >= 0 && y < ROWS) {
+
+    if (grid[y][x] === "blank"){
+      grid[y][x] = randomTile(); 
+    }
+  }
+}
+
+
+function mousePressed() {
+
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  exploreCell(x, y);
 }
